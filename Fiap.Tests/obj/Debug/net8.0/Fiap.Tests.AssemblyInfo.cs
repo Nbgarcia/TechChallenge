4b750d67e9a250b25113e54c79f332605e125407
Natália <nataliab_garcia@hotmail.com>
2024-07-28 10:38:25 -0300

    Commit

--- Fiap.Tests/obj/Debug/net8.0/Fiap.Tests.AssemblyInfo.cs
+++ Fiap.Tests/obj/Debug/net8.0/Fiap.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fiap.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43b3f9918712dcca9d7781d7a8f607648f5ff9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fiap.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fiap.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
